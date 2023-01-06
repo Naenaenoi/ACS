6d4767a9cbb563a37c477158758bd5873fefc816
Tricky Dicky <Omegabanjoes@gmail.com>
2023-01-05 17:32:52 -0800

    space remover now works

--- test.py
+++ test.py
@@ -3,9 +3,22 @@
 
 #Tester for entire equation input instead of individual characters
 import os
+def remover(List, item):
+ 
+    # remove the item for all its occurrences
+    c = List.count(item)
+    for i in range(c):
+        List.remove(item)
+ 
+    return List
+
+
 os.system('cls')
 print("Enter equation")
 print("")
 inp1 = input()
+[*inp1]
 
-print([*inp1])
+res = [ele for ele in inp1 if ele.strip()]
+
+print(res)
