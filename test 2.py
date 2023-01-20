701d612381ca1f057734212c0d062e87e1fcd509
Tricky Dicky <Omegabanjoes@gmail.com>
2023-01-20 09:49:17 -0800

    wrerwh

--- test 2.py
+++ test 2.py
@@ -1,6 +1,7 @@
 import sympy as sp
 import os
 x = sp.Symbol('X')
+y = sp.Symbol('Y')
 os.system('cls')
 
 special_Characters = ['+','-','*','/','^','X','Y']
@@ -10,15 +11,29 @@ print("")
 inp1 = input()
 [*inp1]
 
+def interpreter(entry):
+    counter = 0
+    for element in entry:
+        if element in special_Characters:
+            index1 = entry.index(element)
+            counter += 1
+            
+            
+            
+
+"""
 def dSolver(entry):
     counter = 0
     for element in entry:
         counter += 1
         if element in special_Characters:
             result = element.pop(counter)
+        else:
+            result = entry
 
     modifiedlist = [int(element) for element in result]
-    output = [sp.diff(x**element) for element in modifiedlist]
+    output = [sp.diff(element*x**element) for element in modifiedlist]
     return output
 
 print(dSolver(inp1))
+"""
