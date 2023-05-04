6801362612d4a5ee6f77c59f3e548e60db04b0b1
Tricky Dicky <Omegabanjoes@gmail.com>
2023-05-04 10:47:00 -0700

    3

--- customTkinter test.py
+++ customTkinter test.py
@@ -11,7 +11,54 @@ ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 app = ctk.CTk()  # create CTk window like you do with the Tk window
 app.geometry("700x400")
 
-    
+
+
+dict1 = {'**0':'\u2070',
+         '**1':'\u00b9',
+         '**2':'\u00b2',
+         '**3':'\u00b3',
+         '**4':'\u2074',
+         '**5':'\u2075',
+         '**6':'\u2076',
+         '**7':'\u2077',
+         '**8':'\u2078',
+         '**9':'\u2079'}
+
+dict2 = {'0':'\u2080',
+         '1':'\u2081',
+         '2':'\u2082',
+         '3':'\u2083',
+         '4':'\u2084',
+         '5':'\u2085',
+         '6':'\u2086',
+         '7':'\u2087',
+         '8':'\u2088',
+         '9':'\u2089',
+         '+':'\u208A',
+         '-':'\u208B',
+         '=':'\u208C',
+         '(':'\u208D',
+         ')':'\u208E',
+         'a':'\u2090',
+         'e':'\u2091',
+         'o':'\u2092',
+         'x':'\u2093',
+         'h':'\u2095',
+         'k':'\u2096',
+         'l':'\u2097',
+         'm':'\u2098',
+         'n':'\u2099',
+         'p':'\u209A',
+         's':'\u209B',
+         't':'\u209C'}
+
+def sups(base,x):
+    z = '{}'.format(dict1.get(x))
+    return base + z
+
+def subs(base,x):
+    z = '{}'.format(dict2.get(x))
+    return base + z
 
 def derivative_environment():
     entry = entry_box.get()
@@ -188,6 +235,8 @@ def Integral():
     entry_box.configure(placeholder_text="Input integeral")
     entry_box.update()
     
+app.title("ACS")
+    
 option_frame = ctk.CTkFrame(app)
 option_frame.place(relx=0.5, rely=0.4, anchor = tkinter.CENTER)
 
@@ -196,7 +245,7 @@ derivative_button.grid(row=0, column=0, padx=10, pady=10)
 
 integral_button = ctk.CTkButton(option_frame, text="Integral", command=Integral)
 integral_button.grid(row=0, column=1, padx=10, pady=10)
-    
+
 equation = tkinter.StringVar()
 entry_box = ctk.CTkEntry(app, width=350, height=40, placeholder_text="Select Entrty Mode", textvariable = equation)
 entry_box.place(relx=0.4, rely=0.9, anchor = tkinter.S)
@@ -204,7 +253,7 @@ entry_box.place(relx=0.4, rely=0.9, anchor = tkinter.S)
 solve = ctk.CTkButton(app, text="Solve", command = Derivative)
 solve.place(relx=0.8, rely=0.89, anchor = tkinter.S)
 
-answer_label = ctk.CTkLabel(app, text="")
+answer_label = ctk.CTkLabel(app, text="", font=("TkDefaultFont",20))
 answer_label.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
 app.mainloop()
